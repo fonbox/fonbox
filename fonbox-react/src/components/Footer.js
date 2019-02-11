327b38f98f351292bb156f08c709a37c9d16e2f5
mfekadu <mike.fekadu@gmail.com>
2019-02-11 06:30:42 -0800

    render a Footer in the App
    https://youtu.be/DLX62G4lc44?t=2541

---
+++ fonbox-react/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+    <div>
+        <a href="https://github.com/fonbox/fonbox" id="viewSource">View source code on GitHub</a>
+    </div>
+    </footer>);
+}
+
+export default Footer;
--- fonbox-react/src/index.js
+++ fonbox-react/src/index.js
@@ -1,22 +1,11 @@
-import React from 'react';
+import React from 'react'; // needed to parse & interpret JSX
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import App from './App'; // import 'App' HTML component
 import FonBox from './components/FonBox';
-
 import * as serviceWorker from './serviceWorker';
 
-// render the 'App' componenet into the div with id='root'
-// this is JSX (when HTML goes inside JavaScript)
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// Here is a useful React tutorial:
-// https://www.youtube.com/watch?v=DLX62G4lc44
-
-
-
-ReactDOM.render(<FonBox />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
