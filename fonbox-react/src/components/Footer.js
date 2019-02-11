daee2d801845358f126be67afe7ede1de0a45377
mfekadu <mike.fekadu@gmail.com>
2019-02-11 07:12:57 -0800

    function components can be arrow funcs!
    useful timestamp in tutorial:
    
    https://youtu.be/DLX62G4lc44?t=3411

--- fonbox-react/src/components/Footer.js
+++ fonbox-react/src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function Footer() {
-  return (
-    <footer>
-    <div>
-        <a href="https://github.com/fonbox/fonbox" id="viewSource">View source code on GitHub</a>
-    </div>
-    </footer>);
-}
+const Footer = () => (
+  <footer>
+  <div>
+  <a href="https://github.com/fonbox/fonbox" id="viewSource">View source code on GitHub</a>
+  </div>
+  </footer>);
 
 export default Footer;
