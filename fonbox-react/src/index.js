668d9e005afcf598f7296feb054e9842a9e3333e
mfekadu <mike.fekadu@gmail.com>
2019-02-11 06:15:34 -0800

    display basic html template for fonbox
    functional components practice
    
    https://youtu.be/DLX62G4lc44?t=1475

--- fonbox-react/src/index.js
+++ fonbox-react/src/index.js
@@ -12,15 +12,27 @@ import * as serviceWorker from './serviceWorker';
 // https://www.youtube.com/watch?v=DLX62G4lc44
 
 // Create a 'functional component' that returns some JSX.
-function MyApp() {
-  return (<ul>
-    <li>Coffee</li>
-    <li>Tea</li>
-    <li>Milk</li>
-  </ul>);
+function FonBox() {
+  return (
+    <body>
+      <h1> Hello, welcome to FonBox! </h1>
+      <h2> Put your phone in the box to have fun! </h2>
+      <h3> This app needs your camera and microphone access before the <em>fun times</em> begin </h3>
+      <h4> For the best experience, use Safari on iPhone or Chrome on Android. </h4>
+      <div class="img-div">
+        <img src="https://raw.githubusercontent.com/mfekadu/coursera-test/master/site/img/very_nice.gif" alt="very nice gif borat" />
+      </div>
+      <button class="primary_btn" id="showVideo">Open camera</button>
+      <div id="errorMsg"></div>
+      <video id="gum-local" autoplay playsinline></video>
+      <div>
+          <a href="https://github.com/fonbox/fonbox" id="viewSource">View source code on GitHub</a>
+      </div>
+    </body>
+  );
 }
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<FonBox />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
